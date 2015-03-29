4c14f55bbd641931c73ae3116a70d4d6bb5bc5b5
Louis <spalax@gresille.org>
2015-03-29 12:04:10 +0200

    First version of setup file

--- setup.py
+++ setup.py
@@ -1,1 +1,51 @@
-TODO
+#!/usr/bin/env python
+
+# Copyright 2015 Louis Paternault
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"""Installer"""
+
+from setuptools import setup, find_packages
+import codecs
+import os
+
+def readme():
+    directory = os.path.dirname(os.path.join(
+        os.getcwd(),
+        __file__,
+        ))
+    return codecs.open(os.path.join(directory, "README.rst"), "r", "utf8").read()
+
+setup(
+        name='sphinxcontrib-packages',
+        version="0.0.0",
+        packages=find_packages(),
+        setup_requires=["hgtools"],
+        install_requires=[
+            "sphinx",
+            ],
+        include_package_data=True,
+        author='Louis Paternault',
+        author_email='spalax@gresille.org',
+        description='This packages contains the Packages sphinx extension, which provides directives to display packages installed on the host machine',
+        url='http://git.framasoft.org/spalax/packages',
+        license="GPLv3 or any later version",
+        #test_suite="packages.test.suite",
+        classifiers=[
+            # TODO
+        ],
+        long_description=readme(),
+        zip_safe = True,
+)
