77e21bf11369ea3744cb8bc8d2cc587cea45fee4
Louis <spalax@gresille.org>
2015-04-04 00:07:40 +0200

    Pylint

--- sphinxcontrib/packages/__init__.py
+++ sphinxcontrib/packages/__init__.py
@@ -1,4 +1,23 @@
-# TODO Licence etc.
+# Copyright Louis Paternault 2015
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>. 1
+
+"""This is a sphinx extension providing some directives to show system information.
+
+Its main purpose is to display a list of tools available on readthedocs:
+http://packages.readthedocs.org
+"""
 
 import collections
 import os
@@ -15,18 +34,21 @@ from sphinx.util.nodes import nested_parse_with_titles
 from docutils.parsers.rst import directives
 
 def node_or_str(text):
+    """Return argument, converted to a node if necessary."""
     if isinstance(text, str):
         return nodes.paragraph(text=text)
     else:
         return text
 
 def simple_compound(*items):
+    """Return a compound node."""
     compound = nodes.compound()
     for item in items:
         compound.append(item)
     return compound
 
 def simple_link(text, target):
+    """Returns a link node to `target`, displaying `text`."""
     container = nodes.paragraph()
     reference = nodes.reference("", "", internal=False, refuri=target)
     reference.append(nodes.paragraph(text=text))
@@ -35,7 +57,14 @@ def simple_link(text, target):
 
 
 def simple_table(ncolumns, headers, body):
+    """Return a table node.
+
+    :param int ncolumns: Number of columns.
+    :param list headers: Headers, as a list of nodes (or strings).
+    :param list body: Body, as a list of lists of nodes (or strings).
+    """
     def _build_table_row(data):
+        """Return the node corresponding to a row of the table."""
         row = nodes.row()
         for cell in data:
             entry = nodes.entry()
@@ -47,7 +76,7 @@ def simple_table(ncolumns, headers, body):
     tgroup = nodes.tgroup(cols=2)
     table += tgroup
     for colwidth in [10]*ncolumns:
-        colspec = nodes.colspec(colwidth = colwidth)
+        colspec = nodes.colspec(colwidth=colwidth)
         tgroup += colspec
 
     # HEAD
@@ -65,12 +94,16 @@ def simple_table(ncolumns, headers, body):
     return table
 
 def simple_bulletlist(items):
+    """Return a bullet list nodes of arguments."""
     return nodes.bullet_list("", *[nodes.list_item('', node_or_str(item)) for item in items])
 
 class PlatformDirective(Directive):
+    """Print platform information (processors, architecture, etc.)"""
     has_content = False
 
-    def body(self):
+    @staticmethod
+    def body():
+        """Iterator to the platform information."""
         for attr in [
                 "machine",
                 "platform",
@@ -78,14 +111,17 @@ class PlatformDirective(Directive):
                 "release",
                 "version",
                 "processor",
-                ]:
+            ]:
             yield [attr.replace("_", " ").capitalize(), str(getattr(platform, attr)())]
 
         for attr in [
                 "architecture",
                 "linux_distribution",
-                ]:
-            yield [attr.replace("_", " ").capitalize(), " ".join([str(item) for item in getattr(platform, attr)()])]
+            ]:
+            yield [
+                attr.replace("_", " ").capitalize(),
+                " ".join([str(item) for item in getattr(platform, attr)()]),
+                ]
 
     def run(self):
         return [simple_table(
@@ -95,12 +131,23 @@ class PlatformDirective(Directive):
             )]
 
 class BinDirective(Directive):
+    """Display the list of available binaries."""
 
-    def dirs(self):
+    @staticmethod
+    def dirs():
+        """Iterator over couples `(path, binaries)`.
+
+        - `path` is a path of the ``PATH`` variable;
+        - `binaries` is the list of binaries available in this path.
+        """
         for path in os.getenv("PATH").split(":"):
             binaries = []
             for binary in sorted(os.listdir(os.path.expanduser(os.path.expandvars(path)))):
-                if os.path.isfile(os.path.join(path, binary)) and os.access(os.path.join(path, binary), os.X_OK):
+                if (
+                        os.path.isfile(os.path.join(path, binary))
+                        and
+                        os.access(os.path.join(path, binary), os.X_OK)
+                    ):
                     binaries.append(binary)
             yield (path, binaries)
 
@@ -123,7 +170,9 @@ class BinDirective(Directive):
         return [simple_bulletlist(items)]
 
 def deepdict_factory(depth):
-    """TODO
+    """Return a dict of dicts of dicts of ... of dicts of lists.
+
+    The dicts are :class:`collections.defaultdict`.
 
     >>> d = deepdict_factory(2)()
     >>> type(d)
@@ -136,20 +185,30 @@ def deepdict_factory(depth):
     if depth == 0:
         return list
     else:
-        def deep_dict():
+        def deepdict():
+            """Return a deepdict, less deep than the current one."""
             return collections.defaultdict(deepdict_factory(depth - 1))
-        return deep_dict
+        return deepdict
 
 class CmdDirective(Directive):
-    regexp = ""
+    """Abstract directive that executes a command, and return its output as array(s).
+    """
     command = []
+    regexp = ""
     headers = {}
     sections = []
+    sortkey = None
 
-    def filter(self, match):
+    def filter(self, match): # pylint: disable=no-self-use
+        """Perform some post-processing on matched lines.
+
+        - ``None`` can be returned to dismiss this line.
+        - This function is to be overloaded, if necessary, by subclasses.
+        """
         return match
 
     def _iter_match(self, output):
+        """Iterator over matched lines of the output."""
         compiled_re = re.compile(self.regexp)
         for line in output:
             match = compiled_re.match(line.decode("utf8").strip())
@@ -159,33 +218,38 @@ class CmdDirective(Directive):
                     yield processed_match
 
     def _render_deepdict(self, deepdict):
-        if type(deepdict) == list:
+        """Render a :class:`deepdict`.
+
+        - If it is a deepdict, render it a bullet list of :class:`deepdict`;
+        - if it is a list, render it as a table.
+        """
+        if isinstance(deepdict, list):
             items = dict()
             for item in deepdict:
                 items[item[self.sortkey]] = [item[key] for key in self.headers]
             return simple_table(
-                    len(self.headers),
-                    self.headers.values(),
-                    [items[key] for key in sorted(items.keys())],
-                    )
+                len(self.headers),
+                self.headers.values(),
+                [items[key] for key in sorted(items.keys())],
+                )
         else:
             return simple_bulletlist([
-                    simple_compound(
-                        nodes.paragraph(text=key),
-                        self._render_deepdict(deepdict[key])
-                        )
-                    for key
-                    in sorted(deepdict)
-                    ])
+                simple_compound(
+                    nodes.paragraph(text=key),
+                    self._render_deepdict(deepdict[key])
+                    )
+                for key
+                in sorted(deepdict)
+                ])
 
     def run(self):
         try:
             process = subprocess.Popen(
-                    self.command,
-                    stdin=subprocess.DEVNULL,
-                    stderr=subprocess.DEVNULL,
-                    stdout=subprocess.PIPE,
-                    )
+                self.command,
+                stdin=subprocess.DEVNULL,
+                stderr=subprocess.DEVNULL,
+                stdout=subprocess.PIPE,
+                )
             deepdict = deepdict_factory(len(self.sections))()
             for match in self._iter_match(process.stdout):
                 subdict = deepdict
@@ -201,18 +265,23 @@ class CmdDirective(Directive):
         return [self._render_deepdict(deepdict)]
 
 class DebDirective(CmdDirective):
+    """Display the list of installed debian packages"""
 
-    regexp = r'\t'.join([r'(?P<{}>[^\t]*)'.format(key) for key in ['status', 'section', 'package', 'version', 'homepage', 'summary']])
+    regexp = r'\t'.join([
+        r'(?P<{}>[^\t]*)'.format(key)
+        for key
+        in ['status', 'section', 'package', 'version', 'homepage', 'summary']
+        ])
     command = [
         "dpkg-query",
         "--show",
-        "--showformat=${db:Status-Status}\t${Section}\t${binary:Package}\t${Version}\t${Homepage}\t${binary:Summary}\n",
+        "--showformat=${db:Status-Status}\t${Section}\t${binary:Package}\t${Version}\t${Homepage}\t${binary:Summary}\n", # pylint: disable=line-too-long
         ]
     headers = collections.OrderedDict([
-            ("package_node", "Package name"),
-            ("version", "Version"),
-            ("summary", "Summary"),
-            ])
+        ("package_node", "Package name"),
+        ("version", "Version"),
+        ("summary", "Summary"),
+        ])
     sortkey = "package"
     sections = ["section"]
 
@@ -227,12 +296,13 @@ class DebDirective(CmdDirective):
             return None
 
 class PyDirective(CmdDirective):
+    """Abstract class to display available python modules."""
 
     regexp = r'\t'.join([r'(?P<{}>[^\t]*)'.format(key) for key in ['package', 'version', 'path']])
     headers = collections.OrderedDict([
-            ("package", "Package name"),
-            ("version", "Version"),
-            ])
+        ("package", "Package name"),
+        ("version", "Version"),
+        ])
     sortkey = "package"
     python = ""
 
@@ -243,6 +313,7 @@ class PyDirective(CmdDirective):
 
     @property
     def command(self):
+        """Return the command to perform to list modules."""
         return [
             self.python,
             pkg_resources.resource_filename(
@@ -252,20 +323,27 @@ class PyDirective(CmdDirective):
             ]
 
 class Py3Directive(PyDirective):
+    """Display available python3 modules."""
+
     python = "python3"
 
 class Py2Directive(PyDirective):
+    """Display available python2 modules."""
+
     python = "python2"
 
 class CDirective(CmdDirective):
+    """Display available C libraries."""
+
     regexp = r'^ *(?P<library>[^ ]*) '
     headers = collections.OrderedDict([
-            ("library", "Library"),
-            ])
+        ("library", "Library"),
+        ])
     command = ["/sbin/ldconfig", "-p"]
     sortkey = "library"
 
 def setup(app):
+    """Register directives."""
     app.add_directive('packages:platform', PlatformDirective)
     app.add_directive('packages:bin', BinDirective)
     app.add_directive('packages:deb', DebDirective)
--- sphinxcontrib/packages/data/bin/list_modules.py
+++ sphinxcontrib/packages/data/bin/list_modules.py
@@ -1,26 +1,48 @@
 #!/usr/bin/env python
 
-import types
+# Copyright Louis Paternault 2015
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>. 1
+
+"""List all python modules available to python."""
+
+import logging
+import os
 import pkgutil
 import sys
-import os
+import types
+
+LOGGER = logging.getLogger()
+LOGGER.addHandler(logging.StreamHandler())
 
 VERSION = [
-        "version",
-        "Version",
-        "VERSION",
-        "__version__",
-        "__Version__",
-        "__VERSION__",
-        ]
+    "version",
+    "Version",
+    "VERSION",
+    "__version__",
+    "__Version__",
+    "__VERSION__",
+    ]
 
 def get_version(module):
+    """Guess the version of argument, and returns it, as a string."""
     candidates = [getattr(module, attr) for attr in VERSION if hasattr(module, attr)]
     while candidates:
         first = candidates.pop()
         if callable(first):
             return str(first())
-        elif type(first) == types.ModuleType:
+        elif isinstance(first, types.ModuleType):
             candidates.extend([
                 getattr(first, attr) for attr in VERSION if hasattr(first, attr)
                 ])
@@ -29,6 +51,7 @@ def get_version(module):
     return ""
 
 def module_list():
+    """Yield the list of modules (with version and path)."""
     while True:
         try:
             sys.path.remove(os.getcwd())
@@ -41,9 +64,9 @@ def module_list():
         try:
             module = __import__(name)
             yield name, get_version(module), module.__file__
-        except:
-            pass
+        except Exception as error: # pylint: disable=broad-except
+            LOGGER.warning("Error while importing {}: {}.".format(name, error))
 
 if __name__ == "__main__":
-    for name, version, path in module_list():
-        print("{}\t{}\t{}".format(name, version, path))
+    for package, version, path in module_list():
+        print("{}\t{}\t{}".format(package, version, path))
